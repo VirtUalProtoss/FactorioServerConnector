7f3811e252aa9341a6b3d238c58a2443576f65ad
VirtUal <virtual@mglife.ru>
2024-01-08 17:09:56 +0300

    Add needed intents

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ from config import DISCORD_BOT_TOKEN
 if __name__ == "__main__":
     intents = discord.Intents.default()
     intents.message_content = True
+    intents.members = True
+    intents.reactions = True
     app = DClient(intents=intents)
     app.run(DISCORD_BOT_TOKEN)
